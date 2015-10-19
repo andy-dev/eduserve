37a4ca461e78675f19b8038485f841e7a5c49aa1
andy-dev <andres.macedo@gmail.com>
2015-10-19 08:43:23 -0700

    modified missino to fit screen

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
 $(document).ready(function(){
 
-    window.onresize = function(){
-        var w = window.outerWidth;
-        console.log(w)
-        if(w<530){
-            console.log("hi");
-            $('.mission').css('margin-Top', '-50px');
-        }
-    }
+
+    // jQuery(".mission").fitText(1, { minFontSize: '3px', maxFontSize: '18px' });
 
 
-})
+});
+
+
+
+(function($){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
